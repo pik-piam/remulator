e072745f0bbfdfa2b4b4ea915f23cc2246ffdc38
David Klein <dklein@pik-potsdam.de>
2017-12-14 16:43:50 +0100

    Added dummy description file for test data

---
+++ R/emudata.R
@@ -0,0 +1,9 @@
+#' emudata
+#' 
+#' Example dataset for a regional MAgPIE object
+#' 
+#' 
+#' @name emudata
+#' @return example data as read from MAgPIE report with reduced set of variables
+#' @author David Klein
+NULL
