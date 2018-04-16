6022156a96ca10aa1462015645b404cf853c1399
David Klein <dklein@pik-potsdam.de>
2018-04-16 19:02:57 +0200

    Activated automated generation of regionscode again, since it could be found in the MAgPIE output.

--- R/read_and_combine.R
+++ R/read_and_combine.R
@@ -4,7 +4,7 @@
 #' modelstatus from the fulldata.gdx and appends it to the report. If more than one folder is
 #' given it combines the contents of the multiple reports into a single magpie object and returns it.
 #' If the name of an output file is provided the object is additionally stored in this file. In each of the
-#' folders there must be a report_scenname.mif, a fulldata.gdx, and config.Rdata.
+#' folders there must be a report_scenname.mif, a fulldata.gdx, a spatial_header.rda, and config.Rdata.
 #' 
 #' 
 #' @param list_of_directories Vector of strings providing the folder names from which reports will be read.
@@ -18,29 +18,44 @@
 read_and_combine <- function(list_of_directories, outfile=NULL) {
   cat("Collecting results.\n")
   mag_res <- NULL
-  rcode <- NULL
+  regionscode_previous <- NULL
+  
   for (f in list_of_directories) {
     # extract scenario name from path: any string between "/" and EOL ($) containing "-x" with x being a one or two digit number
     #scenario <- gsub(".*\\/(.*-[0-9]{1,2}$)","\\1",f)
     cfg <- NULL
     load(paste0(f,"/config.Rdata"))
     scenario <- cfg$title
-    #if (!is.null(rcode)) {
-    #  if(rcode != regionscode(paste0("./",cfg$regionmapping))) {
-    #    stop("Region codes are not the same for all runs!")
-    #  }
-    #}
+    
+    load(paste0(f,"/spatial_header.rda"))
+    regionscode_current <- regionscode
+    
+    if (!is.null(regionscode_previous)) {
+      if(regionscode_previous != regionscode_current) {
+        stop("Region codes are not the same for all runs!")
+      }
+    }
+    
+    regionscode_previous <- regionscode_current
+    
     # if region codes of the current and the previous run are identical (stop() above was not executed), keep current one
-    rcode <- "no_regionscode_available" # regionscode(paste0("./",cfg$regionmapping))
-    #cat("Regionscode:",rcode,"\n")
+    cat("Regionscode:",regionscode_current,"\n")
     cat("Reading:",paste0(f,"/report.mif"),"\n")
+    modelstat <- modelstat(paste0(f,"/fulldata.gdx"))
     report <- read.report(paste0(f,"/report.mif"),as.list=FALSE)
+    # if MAgPIE was infeasible in a year the report misses all subsequent years. Thus, the report has less years than 
+    # modelstat that was read from gdx. The gdx always has all years. Therefore, add the missing years to the report, so 
+    # modelstat and report have the same years, and the data from all scenarios have the same years
+    missing_years <- setdiff(getYears(modelstat),getYears(report))
+    if (!identical(missing_years, character(0))) {
+      report <- add_columns(report,dim=2.1,addnm=missing_years)
+    }
     report <- add_columns(report,dim=3.3,addnm="Modelstatus (-)")
-    report["GLO",,"Modelstatus (-)"] <- modelstat(paste0(f,"/fulldata.gdx"))
+    report["GLO",,"Modelstatus (-)"] <- modelstat
     mag_res <- mbind(mag_res,report)
   }
   
-  attr(mag_res,"regionscode") <- rcode
+  attr(mag_res,"regionscode") <- regionscode_current
   
   if (!is.null(outfile)) {
     # save collected results to Rdata file
